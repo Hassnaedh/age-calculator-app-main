bc7b8891ef1bf2a6200a361e20793a0093b4e3f4
Hassnaedh <dehbihassnae@gmail.com>
2024-02-24 20:24:25 +0100

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "purple": "hsl(259, 100%, 65%)",
+          "light-red": "hsl(0, 100%, 67%)",
+        },
+        neutral: {
+          "white": "hsl(0, 0%, 100%)",
+          "off-white": "hsl(0, 0%, 94%)",
+          "light-grey": "hsl(0, 0%, 86%)",
+          "smokey-grey": "hsl(0, 1%, 44%)",
+          "off-black": "hsl(0, 0%, 8%)",
+        },
+      },
+    },
+    fontFamily: {
+      poppins: ["poppins"],
+    },
+  },
+  plugins: [],
+};
